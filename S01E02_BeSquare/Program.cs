a22f31c5400674c76a858b658a6c517c29e2def6
moroz <moroz@axinom.com>
2019-02-14 13:56:15 +0200

    Added QueueUpSomeFactoryJobs() method which works with the ISquareFactory contract

--- S01E02_BeSquare/Program.cs
+++ S01E02_BeSquare/Program.cs
@@ -2,20 +2,26 @@
 
 namespace S01E02_BeSquare
 {
-    internal class Program
+    internal static class Program
     {
+        // A method to queue up factory jobs which works with the ISquareFactory contract
+        static void QueueUpSomeFactoryJobs(ISquareFactory factory)
+        {
+            factory.SquareSize = 3;
+            factory.SquareSize = 3;
+            factory.AddMaterial(30, 30);
+            factory.AddMaterial(3, 30);
+
+            factory.SquareSize = 10;
+            factory.AddMaterial(100, 30);
+        }
         private static void Main(string[] args)
         {
             var factory = new SquareFactory();
             // try-catch block helps to catch exeptions for incorrect input which were implemented in the "SquareFactory" class
             try
             {
-                factory.SquareSize = 3;
-                factory.AddMaterial(30, 30);
-                factory.AddMaterial(3, 30);
-
-                factory.SquareSize = 10;
-                factory.AddMaterial(100, 30);
+                QueueUpSomeFactoryJobs(factory);
 
                 var totalSquares = 0;
 
@@ -31,7 +37,6 @@ namespace S01E02_BeSquare
             {
                 Console.WriteLine(e.Message);
             }
-
 
             Console.WriteLine("Press any key on your keyboard to close the screen...");
             Console.ReadKey();
