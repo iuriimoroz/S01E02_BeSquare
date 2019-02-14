3a4a064b92a7d86fda5a9c2ee0f39af99011951a
moroz <moroz@axinom.com>
2019-02-14 11:42:36 +0200

    Added try-catch block to print out an error message in case of exceptions.

--- S01E02_BeSquare/Program.cs
+++ S01E02_BeSquare/Program.cs
@@ -7,23 +7,31 @@ namespace S01E02_BeSquare
         private static void Main(string[] args)
         {
             var factory = new SquareFactory();
-
-            factory.SquareSize = 3;
-            factory.AddMaterial(30, 30);
-            factory.AddMaterial(3, 30);
-
-            factory.SquareSize = 10;
-            factory.AddMaterial(100, 30);
-
-            var totalSquares = 0;
-
-            foreach (var square in factory.GetSquares())
+            // try-catch block helps to catch exeptions for incorrect input which were implemented in the "SquareFactory" class
+            try
             {
-                totalSquares++;
-                Console.WriteLine($"Created square with size {square.Width}x{square.Height}");
+                factory.SquareSize = 3;
+                factory.AddMaterial(30, 30);
+                factory.AddMaterial(3, 30);
+
+                factory.SquareSize = 10;
+                factory.AddMaterial(100, 30);
+
+                var totalSquares = 0;
+
+                foreach (var square in factory.GetSquares())
+                {
+                    totalSquares++;
+                    Console.WriteLine($"Created square with size {square.Width}x{square.Height}");
+                }
+
+                Console.WriteLine($"Created a total of {totalSquares} squares.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
 
-            Console.WriteLine($"Created a total of {totalSquares} squares.");
 
             Console.WriteLine("Press any key on your keyboard to close the screen...");
             Console.ReadKey();
